3df8e2e55c7199e3f104a081d580ac4b5d2bf4bb
Vivek Hari <vivek.hari@outlook.co.nz>
2017-03-15 18:26:17 +1100

    Compare user input to random target.

--- GuessANumber/Program.cs
+++ GuessANumber/Program.cs
@@ -9,10 +9,40 @@ namespace GuessANumber
             // pick a random number between 1 and 100
             Random rng = new Random();
             int target = rng.Next(100) + 1;
+            
+            // ask the user to guess the number
+            if (Ask(target))
+            {
+                // correct
+                Console.WriteLine("Correct!");
+            }
+            else
+            {
+                // incorrect
+                Console.WriteLine(
+                    $"Incorrect. The correct answer was {target}.");
+            }
 
-            // print target number
-            Console.WriteLine(target);
+            Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
+        }
+
+        static bool Ask(int target)
+        {
+            // ask the user for a number
+            Console.WriteLine("Guess the number (between 1 and 100):");
+            string input = Console.ReadLine();
+
+            // parse input
+            if (int.TryParse(input, out int inputValue))
+            {
+                return inputValue == target;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+                return false;
+            }
         }
     }
 }
