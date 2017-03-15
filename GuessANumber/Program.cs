55657ed0959ca3896e2d27ae2af67c37a0807ec6
Vivek Hari <vivek.hari@outlook.co.nz>
2017-03-15 19:10:12 +1100

    Show whether guess was too low or too high.

--- GuessANumber/Program.cs
+++ GuessANumber/Program.cs
@@ -11,9 +11,25 @@ namespace GuessANumber
             int target = rng.Next(100) + 1;
 
             // keep asking the user until they guess correctly
-            while (!Ask(target))
+            Result result = Ask(target);
+            while (result != Result.Correct)
             {
-                Console.WriteLine("Incorrect.");
+                // display relevant message
+                switch (result)
+                {
+                    case Result.TooLow:
+                        Console.WriteLine("Incorrect (too low).");
+                        break;
+                    case Result.TooHigh:
+                        Console.WriteLine("Incorrect (too high).");
+                        break;
+                    case Result.InvalidInput:
+                        Console.WriteLine("Invalid input.");
+                        break;
+                }
+
+                // ask again
+                result = Ask(target);
             }
 
             // correct
@@ -24,22 +40,32 @@ namespace GuessANumber
             Console.ReadKey();
         }
 
-        static bool Ask(int target)
+        static Result Ask(int target)
         {
             // ask the user for a number
             Console.WriteLine("Guess the number (between 1 and 100):");
             string input = Console.ReadLine();
 
             // parse input
-            if (int.TryParse(input, out int inputValue))
-            {
-                return inputValue == target;
-            }
-            else
-            {
-                Console.WriteLine("Invalid input.");
-                return false;
-            }
+            if (!int.TryParse(input, out int inputValue))
+                return Result.InvalidInput;
+
+            // was the user correct?
+            if (inputValue == target)
+                return Result.Correct;
+
+            // if not, describe why not
+            return inputValue > target
+                ? Result.TooHigh
+                : Result.TooLow;
         }
+    }
+
+    enum Result
+    {
+        Correct,
+        TooLow,
+        TooHigh,
+        InvalidInput,
     }
 }
